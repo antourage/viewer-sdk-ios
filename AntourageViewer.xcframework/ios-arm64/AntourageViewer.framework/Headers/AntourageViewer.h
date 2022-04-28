f42854416e05d910586a4a0b1866725735455c44
Tomash Tsiupiak <mail@tomash345.me>
2022-04-28 20:08:20 +0300

    Version 4.0.0

--- AntourageViewer.xcframework/ios-arm64/AntourageViewer.framework/Headers/AntourageViewer.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for AntourageViewer.
-FOUNDATION_EXPORT double AntourageViewerVersionNumber;
-
-//! Project version string for AntourageViewer.
-FOUNDATION_EXPORT const unsigned char AntourageViewerVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <AntourageViewer/PublicHeader.h>
---
+++ AntourageViewer.xcframework/ios-arm64_x86_64-simulator/AntourageViewer.framework/Headers/Antourage.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for Antourage.
+FOUNDATION_EXPORT double AntourageVersionNumber;
+
+//! Project version string for Antourage.
+FOUNDATION_EXPORT const unsigned char AntourageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Antourage/PublicHeader.h>
